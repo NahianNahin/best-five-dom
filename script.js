3773791b4edb38c6002917ec1ef5a12433467343
NahianNahin <nahian1927040@stud.kuet.ac.bd>
2022-08-22 04:30:59 +0600

    Appending player lists in Select section

---
+++ script.js
@@ -0,0 +1,83 @@
+// Inportant functions 
+
+function appendElement(name){
+    const listContainer = document.getElementById('list-container');
+    const list = document.createElement('li');
+    list.innerText = name;
+    listContainer.appendChild(list);
+}
+function disabledBtn(btn){
+    document.getElementById(btn).disabled = true;
+    document.getElementById(btn).style.background = '#A2A9AF';
+
+}
+
+
+// ADD EVENT IN BUTTONS 
+
+// FOR btn-1 
+document.getElementById('btn-1').addEventListener('click',function(){
+            
+    const listContainer = document.getElementById('list-container');
+        if(listContainer.childElementCount >= 5){
+            alert('you already select 5');
+            return;
+        }
+    appendElement('Lionel Messi');
+    disabledBtn('btn-1');
+})
+// FOR btn-2 
+document.getElementById('btn-2').addEventListener('click',function(){
+            
+    const listContainer = document.getElementById('list-container');
+        if(listContainer.childElementCount >= 5){
+            alert('you already select 5');
+            return;
+        }
+    appendElement('Neymar Jr');
+    disabledBtn('btn-2');
+})
+// FOR btn-3 
+document.getElementById('btn-3').addEventListener('click',function(){
+            
+    const listContainer = document.getElementById('list-container');
+        if(listContainer.childElementCount >= 5){
+            alert('you already select 5');
+            return;
+        }
+    appendElement('Kylian Mbappé');
+    disabledBtn('btn-3');
+})
+// FOR btn-4 
+document.getElementById('btn-4').addEventListener('click',function(){
+            
+    const listContainer = document.getElementById('list-container');
+        if(listContainer.childElementCount >= 5){
+            alert('you already select 5');
+            return;
+        }
+    appendElement('Vítor Machado');
+    disabledBtn('btn-4');
+})
+// FOR btn-5 
+document.getElementById('btn-5').addEventListener('click',function(){
+            
+    const listContainer = document.getElementById('list-container');
+        if(listContainer.childElementCount >= 5){
+            alert('you already select 5');
+            return;
+        }
+    appendElement('Sergio Ramos');
+    disabledBtn('btn-5');
+})
+// FOR btn-6
+document.getElementById('btn-6').addEventListener('click',function(){
+            
+    const listContainer = document.getElementById('list-container');
+        if(listContainer.childElementCount >= 5){
+            alert('you already select 5');
+            return;
+        }
+    appendElement('Renato Sanches');
+    disabledBtn('btn-6');
+})
